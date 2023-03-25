f63df13af29ac2408c5f6d4737ee886367c80f70
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-24 22:20:22 -0300

    added click listener to buttons

--- app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
+++ app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
@@ -2,13 +2,29 @@ package com.ghasty.incognito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private MaterialButton login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(v -> {
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+
+        register.setOnClickListener(v -> {
+            startActivity(new Intent(this, RegisterActivity.class));
+        });
     }
 }

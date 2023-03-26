8ce26835e93256d66f0e4e38fc2979f2ddbe3071
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-26 12:33:48 -0300

    show user data

--- app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
+++ app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.button.MaterialButton;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -22,14 +29,46 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         login.setOnClickListener(v -> {
             startActivity(new Intent(this, LoginActivity.class));
+            finish();
         });
 
         register.setOnClickListener(v -> {
             startActivity(new Intent(this, RegisterActivity.class));
+            finish();
         });
 
         send.setOnClickListener(v -> {
             startActivity(new Intent(this, MainActivity.class));
         });
+
+        try {
+//            getUserData();
+            Log.d("Ghastyy", getUserData());
+        } catch (IOException e) {
+            Log.d("Ghastyy", "No exist" + e.getLocalizedMessage());
+        }
+    }
+
+
+    private String getUserData() throws IOException {
+        FileInputStream fileInputStream = openFileInput("user.txt");
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuilder sb = new StringBuilder();
+        String text = null;
+
+        do {
+            sb.append(text).append("\n");
+        } while ((text = bufferedReader.readLine()) != null);
+
+        if (fileInputStream != null) {
+            try {
+                fileInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
     }
 }

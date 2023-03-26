b1b47da08d658fbcc8cb89605869cc1db1873cda
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-26 12:39:49 -0300

    refactored saveUserData function

--- app/src/main/java/com/ghasty/incognito/Utils.java
+++ app/src/main/java/com/ghasty/incognito/Utils.java
@@ -2,6 +2,21 @@ package com.ghasty.incognito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Utils extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class Utils extends AppCompatActivity {
+    public void saveUserData(String userData) throws IOException {
+        FileOutputStream fileOutputStream = openFileOutput("user.txt", MODE_PRIVATE);
+        fileOutputStream.write(userData.getBytes());
+
+        if (fileOutputStream != null) {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }

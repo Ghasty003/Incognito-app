59729231b56adc753c7653c757ffca896a8673f4
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-26 21:59:07 -0300

    refacotored

--- app/src/main/java/com/ghasty/incognito/MainActivity.java
+++ app/src/main/java/com/ghasty/incognito/MainActivity.java
@@ -2,13 +2,43 @@ package com.ghasty.incognito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+
+        String userData = getIntent().getStringExtra("userData");
+        Log.d("Ghastyy", userData);
+
+
+        logout.setOnClickListener(v -> {
+            clearUserData();
+            startActivity(new Intent(this, SplashScreenActivity.class));
+        });
+    }
+
+    private void clearUserData() {
+        FileOutputStream fileOutputStream;
+
+        try {
+            fileOutputStream = openFileOutput("user.txt", MODE_PRIVATE);
+            String text = "";
+            fileOutputStream.write(text.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

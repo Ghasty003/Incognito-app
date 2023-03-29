16d08066d814fd34023e6c8c113a8f2cfb8fd2f5
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-29 10:03:17 -0300

    created input dialog

--- app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
+++ app/src/main/java/com/ghasty/incognito/SplashScreenActivity.java
@@ -1,18 +1,14 @@
 package com.ghasty.incognito;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.InputType;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -36,7 +32,32 @@ public class SplashScreenActivity extends AppCompatActivity {
         });
 
         send.setOnClickListener(v -> {
-            startActivity(new Intent(this, SendMessageActivity.class));
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+            dialog.setTitle("Username of the recipient?");
+
+            EditText editText = new EditText(this);
+            editText.setInputType(InputType.TYPE_CLASS_TEXT);
+
+            dialog.setView(editText);
+
+            dialog.setPositiveButton("Done", ((dialogInterface, i) -> {
+                String username = editText.getText().toString();
+
+                if (username.equals("")) {
+                    editText.setError("Username can't be empty");
+                    return;
+                }
+
+                Intent intent = new Intent(this, SendMessageActivity.class);
+                intent.putExtra("username", username);
+                startActivity(intent);
+            }));
+
+            dialog.setNegativeButton("Cancel", ((dialogInterface, i) -> {
+                dialogInterface.cancel();
+            }));
+
+            dialog.show();
         });
     }
 }

92e0e4ee20425cf25e7c17b881b3fd15e7ade74a
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-03-29 10:11:23 -0300

    added username to view

--- app/src/main/java/com/ghasty/incognito/SendMessageActivity.java
+++ app/src/main/java/com/ghasty/incognito/SendMessageActivity.java
@@ -3,12 +3,28 @@ package com.ghasty.incognito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SendMessageActivity extends AppCompatActivity {
+    private TextView userView;
+    private MaterialButton sendMessage;
+    private EditText message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        userView = findViewById(R.id.username);
+        sendMessage = findViewById(R.id.mb_send);
+        message = findViewById(R.id.et_message);
+
+        String username = getIntent().getStringExtra("username");
+        String userText = "Send a secret message to " + username;
+
+        userView.setText(userText);
     }
 }
